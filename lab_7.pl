451ff87847227b5dfc323940def573530ca7c4ca
Ponchu24 <panh100@gmail.com>
2021-03-26 13:29:22 +0300

    lab7#3

--- lab_7.pl
+++ lab_7.pl
@@ -14,7 +14,7 @@ pr1:-read_str(List,Length),write_str(List),write(", "),write_str(List),write(", 
 
 pr2:-read_str(List,_),count_words(List,0,K),write(K),!.
 count_words([],K,K):-!.
-count_words(Str,I,K):-skip_space_marks(Str,Str1),catch_word(Str1,Word,Str2),Word\=[],I1 is I+1,count_words(Str2,I1,K),!.
+count_words(Str,I,K):-skip_space_marks(Str,Str1),get_word(Str1,Word,Str2),Word\=[],I1 is I+1,count_words(Str2,I1,K),!.
 count_words(_,K,K).
 
 skip_space([32|T],Str1):-skip_space(T,Str1),!.
@@ -23,8 +23,18 @@ skip_space(Str1,Str1).
 skip_space_marks([H|T],Str1):-(H=32;H=40;H=41;H=43;H=44;H=46;H=47),skip_space_marks(T,Str1),!.
 skip_space_marks(Str1,Str1).
 
-catch_word([],[],[]):-!.
-catch_word(Str,Word,Str2):-catch_word(Str,[],Word,Str2).
-catch_word([],Word,Word,[]).
-catch_word([H|T],Word,Word,T):-(H=32;H=40;H=41;H=43;H=44;H=46;H=47),!.
-catch_word([H|T],Word,Word1,Str2):-append(Word,[H],Word2),catch_word(T,Word2,Word1,Str2).
+get_word([],[],[]):-!.
+get_word(Str,Word,Str2):-get_word(Str,[],Word,Str2).
+get_word([],Word,Word,[]).
+get_word([H|T],Word,Word,T):-(H=32;H=40;H=41;H=43;H=44;H=46;H=47),!.
+get_word([H|T],Word,Word1,Str2):-append(Word,[H],Word2),get_word(T,Word2,Word1,Str2).
+
+%3
+
+pr3:-read_str(Str,_),pr3(Str,0,[],Res),write_str(Res).
+pr3([],_,Res,Res):-!.
+pr3(Str,Cur_count,Cur_word,Res):-skip_space_marks(Str,Str1),get_word(Str1,Word,Str2),numb_same_words(Word,Str1,Count),(Count>Cur_count->pr3(Str2,Count,Word,Res);pr3(Str2,Cur_count,Cur_word,Res)).
+numb_same_words(Word,Str,Count):-numb_same_words(Word,Str,1,Count),!.
+numb_same_words(_,[],Count,Count):-!.
+numb_same_words(Word,Str,Cur_count,Count):-skip_space_marks(Str,Str1),get_word(Str1,Word1,Str2),(Word=Word1->Cur_count1 is Cur_count+1,numb_same_words(Word,Str2,Cur_count1,Count);numb_same_words(Word,Str2,Cur_count,Count)).
+
