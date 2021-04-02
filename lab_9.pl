ede50ed600fa1d6f3052a0fe876511435a6e84db
Ponchu24 <panh100@gmail.com>
2021-04-03 00:29:54 +0300

    lab9#1_2

--- lab_9.pl
+++ lab_9.pl
@@ -8,7 +8,19 @@ write_str([H|Tail]):-put(H),write_str(Tail).
 in_list([El|_],El).
 in_list([_|T],El):-in_list(T,El).
 
-%1.1
+in_list_exlude([El|T],El,T).
+in_list_exlude([H|T],El,[H|T1]):-in_list_exlude(T,El,T1).
+
+%1.1. Дано множество. Построить все размещения с повторениями по k
+% элементов.
+
 pr1_1:-read_str(A,_),read(K),k_perm_rep(A,K,[]).
 k_perm_rep(_,0,Perm):-write_str(Perm),nl,!,fail.
 k_perm_rep(A,N,Perm):-in_list(A,El),N1 is N-1,k_perm_rep(A,N1,[El|Perm]).
+
+%1.2. Дано множество. Построить все перестановки.
+
+pr1_2:-read_str(A,_),perm(A,[]).
+perm([],Perm):-write_str(Perm),nl,!,fail.
+perm(A,Perm):-in_list_exlude(A,El,A1),perm(A1,[El|Perm]).
+
